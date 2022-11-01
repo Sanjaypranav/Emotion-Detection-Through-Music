c3f51afc5f5444efcf2c46e52669255e45b449ce
Sanjaypranav <sanjaypriya195@gmail.com>
2022-11-01 22:33:38 +0530

    mergedd video and audio edm

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,9 @@
 """Predict both images and audio"""
 import os
+import subprocess
 import argparse
 import numpy as np
-
+import math
 import cv2
 import moviepy.editor as mp
 from rich import print as rprint
@@ -33,6 +34,33 @@ def video_breaker(video_path: str, save_dir: str = "results/frames/") -> str:
     return "Frames Saved"
 
 
+
+
+classes = {
+    0 : "anger",
+    1 : "bored",
+    2 : "excited",
+    3 : "fear",
+    4 : "happy",
+    5 : "relax",
+    6 : "sad",
+    7 : "worry"
+}
+
+global emotions , path
+emotions = [0, 0, 0, 0, 0, 0 ,0, 0]
+path = 'src/yolov5/runs/detect/exp17/labels'
+def read_text_file(file_path: str) -> str:
+    for file in os.listdir(file_path):
+        # print(file)
+        with open(f'{file_path}/{file}',"r") as f:
+            emotions[int(f.read(1))] += 1
+    return  f"Done {file_path}"
+
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--video', help='path to video file', required=True)
@@ -48,8 +76,15 @@ if __name__ == '__main__':
     rprint(f"[bold green]Extracting audio from {video_path}[/bold green]")
     print(audio_extractor(video_path, save_dir))
     print(video_breaker(video_path))
-
     os.system(f"python src/detect.py -m {opt.audio_model} -f {opt.save_dir}/test.mp3 ")
+    # # capture bash output to a text file
     os.system(
-        f"python src/yolov5/detect.py --weights src/weights/best.pt --img 640 --conf 0.40 --source results/frames/ --data src/config/edm8.yaml  --save-txt")
+        "python3 'src/yolov5/detect.py' --weights 'src/weights/best.pt' --source 'results/frames/' --data src/config/edm8.yaml --save-txt")
+    read_text_file(path)
+    percentage = [i/sum(emotions) for i in emotions]
+    rprint(percentage)
+    # print(max(percentage))
+    print(classes[percentage.index(max(percentage))])
     print("Done")
+
+    # read_and_count_output_file()
